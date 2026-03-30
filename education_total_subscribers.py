a3ac864dcb8693289061772a3c3b31fd1b080870
Hanspeter <hansdietiker2012@gmail.com>
2026-03-30 17:12:24 -0300

    refactor<total_subscribers> refactor dashboard style

--- education_total_subscribers.py
+++ education_total_subscribers.py
@@ -1,17 +1,25 @@
 import pandas as pd
 import plotly.express as px
 
-
-df_education = pd.read_csv("samples/MICRODADOS_CADASTRO_CURSOS_2024.CSV", 
+#Total de Inscritos
+df_education = pd.read_csv("/content/samples/MICRODADOS_CADASTRO_CURSOS_2024.CSV", 
                            sep=';', encoding='iso-8859-1', low_memory=False)
+
 
 total_inscritos = df_education['QT_INSCRITO_TOTAL'].sum()
 
-grafico = px.bar(x=["Total de Inscritos "], 
-             y=[total_inscritos],
-             title="Inscritos Totais - Censo 2024",
-             labels={'y': 'Quantidade de Pessoas', 'x': '2024'},
-             text_auto=',.0f')
+total_inscritos = {'Ano':['2024'], 'Total': [total_inscritos]}
 
-grafico.update_traces(marker_color='darkviolet', width=0.1) 
+
+grafico = px.bar(total_inscritos, 
+                 x ='Ano', 
+                 y='Total',
+                 title="Inscritos Totais - Censo 2024",
+                 labels={'y': 'Quantidade de Pessoas', 'x': 'Censo'},
+                 text_auto=',.0f') 
+
+
+grafico.update_traces(marker_color='CornflowerBlue', width=0.1) 
+grafico.update_layout(yaxis_tickformat=',.0f')
+
 grafico.show()
